8f7c4ceb22c5d1ffa7955f643d994f3598238419
Asher Mathews <msasher123@gmail.com>
2022-05-13 07:05:19 +0530

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,6 +5,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(child: Text("child")),
+    );
   }
 }
